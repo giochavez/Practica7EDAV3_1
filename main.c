8312dddac794e125620178aafe95798b4ceeeaf6
giochavez <54290463+giochavez@users.noreply.github.com>
2020-11-20 19:39:07 -0600

    Update main.c

--- main.c
+++ main.c
@@ -25,13 +25,13 @@ int main(){
             case 2:
                 system("cls");
                 system("color 0E");
-                printf("MOSTRANDO H%cROES\n", 144);
+                printf("MOSTRANDO H%cROES:\n\n", 144);
                 recorre(&misHeroes, imprime); //recorremos la lista de misHeroes y los imprimimos
                 system("PAUSE");
                 break;
             case 3:
                 system("cls");
-                system("color 01");
+                system("color 0A");
                 printf("Mostrar h%croes mayor a...\n", 130);
                 printf("H%croes mayores a: ", 130); //Preguntamos al usuario a partir de que nivel de vida quiere ver los heroes
                 scanf("%d", &k); //Le asignamos el nivel de vida a k
@@ -40,6 +40,13 @@ int main(){
                 system("PAUSE");
                 break; //(...)de los personajes y solo imprimimos los que cumplan con el hecho de ser mayores al dato ingresado por el usuario
             case 4:
+                system("cls");
+                system("color 0B");
+                printf("H%cROES DE MAYOR A MENOR:\n\n", 130); //No sabia si queria ordenarlo de mayor a menor o solo los mayores a, así que hice ambas
+                recorreParaAcomodo(&misHeroes,comparaVida,imprime); //Recorremos la lista, coparamos las vidas por burbuja, e imprimimos la lista final
+                system("PAUSE");
+                break;
+            case 5:
                 system("cls");
                 system("color 0D");
                 ataqueAyB(&misHeroes); //Recibe la lista de heroes para buscar a los oponentes
@@ -51,7 +58,8 @@ int main(){
                 system("color 0C");
                 printf("***************************************\n\n");
                 printf("Hasta luego!!!!\n");
-                printf("Gracias por usar el programa de H%croes\n\n", 130);
+                printf("Gracias por usar el programa de H%croes\n", 130);
+                printf("Hecho con colores y acentos, created by GIO\n\n");
                 printf("***************************************\n\n");
                break;
             default: printf("Opci%cn no reconocida %d\n", 163, opcion);
